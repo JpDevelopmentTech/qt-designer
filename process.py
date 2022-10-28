1beda577927b4064dd3e6114dc0bc3a7f84d2465
Jesus Pineda <jesuspineda18@outlook.es>
2022-10-28 01:45:10 -0500

    primer commit

---
+++ process.py
@@ -0,0 +1,14 @@
+data = []
+
+class process:
+
+    def __init__(self) -> None:
+        pass
+    
+    def agregarTrabajo(self, trabajo):
+        data.append(trabajo)
+
+    def mostrarData():
+        print(data[0])
+
+            
